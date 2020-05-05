ed15e181a013f9f9f2a15a136fae4ec460ade746
jebitok-dev <sharonkosgei4@gmail.com>
2020-05-05 18:03:57 +0300

    algotrithms

---
+++ strut/algorithms/index.js
@@ -0,0 +1,12 @@
+function findLongestWordLength(str) {
+  var words = str.split(' ');
+  var maxLength = 0;
+
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
