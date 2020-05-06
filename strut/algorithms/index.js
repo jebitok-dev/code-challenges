08b24aed6eda088237bf6b2a3ca5d624e33e1b1f
jebitok-dev <sharonkosgei4@gmail.com>
2020-05-06 19:03:16 +0300

    fcc challenges

--- strut/algorithms/index.js
+++ strut/algorithms/index.js
@@ -10,3 +10,32 @@ function findLongestWordLength(str) {
 
   return maxLength;
 }
+//TitleCase first letter of string
+function titleCase(str) {
+  return str.toLowerCase().replace(/(^|\s)\S/g, L => L.toUpperCase());
+}
+
+titleCase("I'm a little tea pot");
+//splice & slice arrays
+function frankenSplice(arr1, arr2, n) {
+let locArray = arr2.slice();
+for (let i = 0; i < arr1.length; i++) {
+  locArray.splice(n, 0, arr1[i]);
+  n++;
+}
+return locArray;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+//falsey
+function bouncer(arr) {
+  let newArray = [];
+  for ( let i = 0; i < arr.length; i++) {
+    if (arr[i]) newArray.push(arr[i]);
+  }
+  return newArray;
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+
