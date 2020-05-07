687d9cb4bc66937b87c94afb5a4ae9aeedb4bc4d
jebitok-dev <sharonkosgei4@gmail.com>
2020-05-07 13:31:57 +0300

    oop

--- index.js
+++ index.js
@@ -18,3 +18,18 @@ for (let property in beagle) {
     prototypeProps.push(property);
   }
 }
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Only change code below this line
+numLegs: 4,
+eat: function() {
+console,log("nom nom nom");
+},
+describe: function() {
+  console.log("My name is " + this.name)
+}
+};
