cb748783a56883572963fda74b49aeed8658a3da
jebitok-dev <sharonkosgei4@gmail.com>
2020-05-12 06:48:53 +0300

    algorithms

--- oop/index.js
+++ oop/index.js
@@ -33,3 +33,59 @@ return [].concat(arr).sort(function (a, b) {
   // Only change code above this line
 }
 nonMutatingSort(globalArray);
+
+function sentensify(str) {
+  // Only change code below this line
+return str.split(/\W/).join(" ");
+
+  // Only change code above this line
+}
+sentensify("May-the-force-be-with-you");
+
+var globalTitle = "Winter Is Coming";
+
+//currying
+// Only change code below this line
+function urlSlug(title) {
+return title
+  .split(/\W/)
+  .filter(obj => {
+    return obj !== "";
+  })
+  .join("-")
+  .toLowerCase();
+
+}
+// Only change code above this line
+function sumAll(arr) {
+  var max = Math.max(arr[0], arr[1]);
+  var min = Math.min(arr[0], arr[1]);
+  var temp = 0;
+  for (var i = min; i <= max; i++) {
+    temp += i;
+  }
+  return temp;
+}
+
+sumAll([1, 4]);
+
+function spinalCase(str) {
+var regex = /\s+|_+/g;
+str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+return str.replace(regex, "-").toLowerCase();
+}
+
+spinalCase('This Is Spinal Tap');
+
+function translatePigLatin(str) {
+  let consonantRegex = /^[^aeiou]+/;
+  let myConsonants = str.match(consonantRegex);
+  return myConsonants !== null
+    ? str
+        .replace(consonantRegex, "")
+        .concat(myConsonants)
+        .concat("ay")
+    : str.concat("way");
+}
+
+translatePigLatin("consonant");
