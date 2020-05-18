1748e4f5312230d6a7e8a7a4106891128adae307
jebitok-dev <sharonkosgei4@gmail.com>
2020-05-18 11:46:33 +0300

    constructor & math algo

--- strut/ES6/scope.js
+++ strut/ES6/scope.js
@@ -59,3 +59,68 @@ makeServerRequest.then(result => {
 makeServerRequest.then(error => {
   console.log(error);
 });
+
+function addTogether() {
+  var args = arguments;
+  var a = args[0];
+  var b = args[1];
+function isNum(num) {
+  return Number.isInteger(num);
+}
+
+if(isNum(a)) {
+  if(isNum(b))
+  return a + b;
+  else if(!b)
+  return function(b) {
+    if(isNum(b))
+  return a + b;
+  }
+}
+}
+
+addTogether(2,3);
+
+
+//Algorithm scritping : Make Person
+var Person = function(firstAndLast) {
+  let nameArr = firstAndLast.split(" ");
+
+  this.getFirstName = function() {
+    let firstName = nameArr[0];
+    return firstName;
+  };
+  this.getLastName = function() {
+    let lastName = nameArr[1]
+    return lastName;
+  };
+  this.getFullName = function() {
+    const fullName = nameArr.join(" ");
+    return fullName;
+  };
+  this.setFirstName = function(firstName) {
+    nameArr[0] = firstName;
+  };
+  this.setLastName = function(lastName) {
+    nameArr[1] = lastName;
+  };
+  this.setFullName = function(firstAndLast) {
+    nameArr = firstAndLast.split(" ");
+  }
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
+
+//map of debris Math.
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  for( var i = 0; i < arr.length; i++) {
+    arr[i].orbitalPeriod = Math.round((2 * Math.PI)* Math.sqrt(Math.pow((earthRadius+ arr[i].avgAlt),3)/GM))
+    delete arr[i].avgAlt;
+  }
+  return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
